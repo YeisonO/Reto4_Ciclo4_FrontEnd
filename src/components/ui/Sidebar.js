450a4868f2a8b902cbfae0954d9b922480eae7d7
YeisonO <ingyeisonorozco@gmail.com>
2021-12-19 12:41:27 -0500

    Initial commit from Ciclo 4 Reto 4 React FrontEnd

---
+++ src/components/ui/Sidebar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+const Sidebar = () => {
+       return( 
+        <div className="md:w-2/6 xl:w-1/5 bg-gray-800">
+            <div className="p-6">
+                <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">Contra Corriente LTDA</p>
+                <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">Empresa de Tecnologia</p>
+
+                <p className="mt-3 text-gray-600">Lista de Opciones</p>
+
+                <nav className="mt-10">
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" exact="true" to="/">Login</NavLink>
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" exact="true" to="/ordenes">Ordenes</NavLink>
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" exact="true" to="/productos">Productos</NavLink>
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" exact="true" to="/usuarios">Usuarios</NavLink>
+                </nav>
+            </div>
+        </div>
+     );
+}
+ 
+export default Sidebar;
